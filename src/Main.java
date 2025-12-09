fbc57db53392e9ea9be1cb31340e2d3b344ff231
MyNPostr <arystan.kamken@gmail.com>
2025-12-09 20:13:41 +0500

    First New core class Pet and demonstrating how it works in the main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Everyone!\nThis application is designed for veterinary clinics to manage clients, pets, appointments, and medical records.\nIt helps streamline workflow and improve service efficiency.");
+        System.out.println("Hello Everyone!\nThis application is designed for veterinary clinics to manage clients, veterinarians, pets, appointments, and treatments.");
+        Pet mops = new Pet("Ars", "Мопс", 4, false);
+
+        System.out.println(mops.getAge());
+        System.out.println(mops.getName());
+        System.out.println(mops.isVaccinated());
+        mops.setName("Arliondr");
+        mops.setAge(6);
+        mops.vaccinate();
+        System.out.println(mops.getAge());
+        mops.birthday();
+        System.out.println(mops.getAge());
+        System.out.println(mops.getName());
+        System.out.println(mops.isVaccinated());
+        mops.vaccinate();
     }
 }
---
+++ src/Pet.java
@@ -0,0 +1,34 @@
+public class Pet {
+    private String name;
+    private String species;
+    private int age;
+    private boolean vaccinated;
+
+    public Pet(String name, String species, int age, boolean vaccinated) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+        this.vaccinated = vaccinated;
+    }
+    public String getName() {return name;}
+    public String getSpecies() {return species;}
+    public int getAge() {return age;}
+    public boolean isVaccinated() {return vaccinated;}
+    public void setName(String name) {this.name = name;}
+    public void setAge(int age) {this.age = age;}
+
+    public void birthday(){
+        this.age++;
+        System.out.printf("%s turned %d!\n", this.name, this.age);
+    }
+
+    public void vaccinate(){
+        if (!vaccinated){
+            this.vaccinated = true;
+            System.out.printf("%s has been vaccinated!\n", this.name);
+        }
+        else{
+            System.out.printf("%s is already vaccinated!\n", this.name);
+        }
+    }
+}
