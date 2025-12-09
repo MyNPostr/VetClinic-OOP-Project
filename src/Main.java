62990891c55243f6f14f724fe321ecae97f3c254
MyNPostr <arystan.kamken@gmail.com>
2025-12-09 19:20:21 +0500

    changed output message to description of the project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-void main() {
-    System.out.println("Hello World!");
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone!\nThis application is designed for veterinary clinics to manage clients, pets, appointments, and medical records.\nIt helps streamline workflow and improve service efficiency.");
+    }
 }
